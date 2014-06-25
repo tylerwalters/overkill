ddce06f293fac27840e36cb43b2941facc9a93d5
Tyler Walters <tyler@tylerwalters.com>
2014-06-24 23:05:13 -0700

    Changing board build functionality to adapt to a landscape or portrait screen

--- app/js/script.js
+++ app/js/script.js
@@ -3,17 +3,66 @@
  * Builds playing grid in .board div.
  */
 
-var board = $('.board');
+var board = $('.board'),
+		portraitArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'BB', 'CC', 'DD', 'EE', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL', 'MM', 'NN', 'OO', 'PP', 'QQ', 'RR', 'SS', 'TT', 'UU', 'VV', 'WW', 'XX', 'YY', 'ZZ', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE', 'FFF', 'GGG', 'HHH', 'III', 'JJJ', 'KKK', 'LLL', 'MMM', 'NNN', 'OOO', 'PPP', 'QQQ', 'RRR', 'SSS', 'TTT', 'UUU', 'VVV', 'WWW', 'XXX', 'YYY', 'ZZZ', 'AAAA', 'BBBB'],
+		landscapeArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'BB', 'CC', 'DD', 'EE', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL', 'MM', 'NN', 'OO', 'PP', 'QQ', 'RR', 'SS', 'TT', 'UU', 'VV', 'WW', 'XX', 'YY', 'ZZ', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE', 'FFF', 'GGG', 'HHH', 'III', 'JJJ', 'KKK', 'LLL', 'MMM', 'NNN', 'OOO', 'PPP', 'QQQ', 'RRR', 'SSS', 'TTT', 'UUU', 'VVV', 'WWW', 'XXX', 'YYY', 'ZZZ', 'AAAA', 'BBBB'],
+		i = 0,
+		j = 0,
+		space, rows, columns;
 
-for (var i = 1; i <= 10; i++) {
-	board.append('<div class="row row-' + i + '"></div>');
-	for (var j = 1; j <= 8; j++) {
-		if ((i % 2 !== 0 && j % 2 !== 0) || (i % 2 === 0 && j % 2 === 0)) {
-			$('.row-' + i).append('<div class="white space ' + i + '-' + j + '"></div>');
-		} else {
-			$('.row-' + i).append('<div class="black space ' + i + '-' + j + '"></div>');
+function buildBoard() {
+	if (orientation === 'portrait') {
+		rows = 10;
+		columns = 8;
+
+		board.css('height', '100%');
+
+
+
+		board.css('width', space.height() * columns + 10);
+
+		for (i; i < 80; i++) {
+			if ((i % 2 !== 0)) {
+				board.append('<div class="white space ' + portraitArr[i] + '"></div>');
+			} else {
+				board.append('<div class="black space ' + portraitArr[i] + '"></div>');
+			}
 		}
 	}
+	else if (orientation === 'landscape') {
+		rows = 8;
+		columns = 10;
+		
+		for (i; i < 80; i++) {
+			if ((i % 2 !== 0)) {
+				board.append('<div class="white space ' + landscapeArr[i] + '"></div>');
+			} else {
+				board.append('<div class="black space ' + landscapeArr[i] + '"></div>');
+			}
+		}
+
+
+		board.css('width', '100%');
+
+		space = $('.space');
+
+		space.css({width: (board.width() - 10) / 10});
+		space.css({height: (board.width() - 10) / 10});
+
+		board.css('height', space.height() * rows + 10);
+	}
+
+}
+var orientation = 'portrait';
+
+function detectOrientation() {
+  orientation = $(document).width() > $(document).height() ? 'landscape' : 'portrait';
 }
 
-board.css('width', $('.row').height() * 8);
+window.addEventListener('resize', function(event){
+  detectOrientation();
+  buildBoard();
+});
+
+detectOrientation();
+buildBoard();
