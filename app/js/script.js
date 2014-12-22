2bc574f8ca761ada3f3d378327cbe24328de3e0d
Tyler Walters <tyler@tylerwalters.com>
2014-12-22 12:14:40 -0700

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -8,14 +8,14 @@
 function buildBoard(view) {
 			// var board is assigned to the div with the .board class
 	var board = $('.board'),
-		// var portraitArr has the classes assigned to each space on the obard in the correct order for portrait orientation
-		portraitArr = ['a-1', 'b-1', 'c-1', 'd-1', 'e-1', 'f-1', 'g-1', 'h-1', 'i-1', 'j-1', 'a-2', 'b-2', 'c-2', 'd-2', 'e-2', 'f-2', 'g-2', 'h-2', 'i-2', 'j-2', 'a-3', 'b-3', 'c-3', 'd-3', 'e-3', 'f-3', 'g-3', 'h-3', 'i-3', 'j-3', 'a-4', 'b-4', 'c-4', 'd-4', 'e-4', 'f-4', 'g-4', 'h-4', 'i-4', 'j-4', 'a-5', 'b-5', 'c-5', 'd-5', 'e-5', 'f-5', 'g-5', 'h-5', 'i-5', 'j-5', 'a-6', 'b-6', 'c-6', 'd-6', 'e-6', 'f-6', 'g-6', 'h-6', 'i-6', 'j-6', 'a-7', 'b-7', 'c-7', 'd-7', 'e-7', 'f-7', 'g-7', 'h-7', 'i-7', 'j-7', 'a-8', 'b-8', 'c-8', 'd-8', 'e-8', 'f-8', 'g-8', 'h-8', 'i-8', 'j-8'],
-		// var landscapeArr has the classes assigned to each space on the obard in the correct order for landscape orientation
-		landscapeArr = ['a-8', 'b-8', 'c-8', 'd-8', 'e-8', 'f-8', 'g-8', 'h-8', 'i-8', 'j-8', 'a-7', 'b-7', 'c-7', 'd-7', 'e-7', 'f-7', 'g-7', 'h-7', 'i-7', 'j-7', 'a-6', 'b-6', 'c-6', 'd-6', 'e-6', 'f-6', 'g-6', 'h-6', 'i-6', 'j-6', 'a-5', 'b-5', 'c-5', 'd-5', 'e-5', 'f-5', 'g-5', 'h-5', 'i-5', 'a-4', 'b-4', 'c-4', 'd-4', 'e-4', 'f-4', 'g-4', 'h-4', 'i-4', 'j-4', 'a-3', 'b-3', 'c-3', 'd-3', 'e-3', 'f-3', 'g-3', 'h-3', 'i-3', 'j-3', 'a-2', 'b-2', 'c-2', 'd-2', 'e-2', 'f-2', 'g-2', 'h-2', 'i-2', 'j-2', 'a-1', 'b-1', 'c-1', 'd-1', 'e-1', 'f-1', 'g-1', 'h-1', 'i-1', 'j-1'],
-		// var i is used to iterate over each space on the board to create the divs and assign the classes
-		i = 0,
-		// var space will hold the divs with the class space; var rows will be assigned the numbers of rows on the board; var columns will be assigned the number of columns on the board;
-		space, rows, columns;
+			// var portraitArr has the classes assigned to each space on the obard in the correct order for portrait orientation
+			portraitArr = ['a-1', 'b-1', 'c-1', 'd-1', 'e-1', 'f-1', 'g-1', 'h-1', 'i-1', 'j-1', 'a-2', 'b-2', 'c-2', 'd-2', 'e-2', 'f-2', 'g-2', 'h-2', 'i-2', 'j-2', 'a-3', 'b-3', 'c-3', 'd-3', 'e-3', 'f-3', 'g-3', 'h-3', 'i-3', 'j-3', 'a-4', 'b-4', 'c-4', 'd-4', 'e-4', 'f-4', 'g-4', 'h-4', 'i-4', 'j-4', 'a-5', 'b-5', 'c-5', 'd-5', 'e-5', 'f-5', 'g-5', 'h-5', 'i-5', 'j-5', 'a-6', 'b-6', 'c-6', 'd-6', 'e-6', 'f-6', 'g-6', 'h-6', 'i-6', 'j-6', 'a-7', 'b-7', 'c-7', 'd-7', 'e-7', 'f-7', 'g-7', 'h-7', 'i-7', 'j-7', 'a-8', 'b-8', 'c-8', 'd-8', 'e-8', 'f-8', 'g-8', 'h-8', 'i-8', 'j-8'],
+			// var landscapeArr has the classes assigned to each space on the obard in the correct order for landscape orientation
+			landscapeArr = ['a-8', 'b-8', 'c-8', 'd-8', 'e-8', 'f-8', 'g-8', 'h-8', 'i-8', 'j-8', 'a-7', 'b-7', 'c-7', 'd-7', 'e-7', 'f-7', 'g-7', 'h-7', 'i-7', 'j-7', 'a-6', 'b-6', 'c-6', 'd-6', 'e-6', 'f-6', 'g-6', 'h-6', 'i-6', 'j-6', 'a-5', 'b-5', 'c-5', 'd-5', 'e-5', 'f-5', 'g-5', 'h-5', 'i-5', 'a-4', 'b-4', 'c-4', 'd-4', 'e-4', 'f-4', 'g-4', 'h-4', 'i-4', 'j-4', 'a-3', 'b-3', 'c-3', 'd-3', 'e-3', 'f-3', 'g-3', 'h-3', 'i-3', 'j-3', 'a-2', 'b-2', 'c-2', 'd-2', 'e-2', 'f-2', 'g-2', 'h-2', 'i-2', 'j-2', 'a-1', 'b-1', 'c-1', 'd-1', 'e-1', 'f-1', 'g-1', 'h-1', 'i-1', 'j-1'],
+			// var i is used to iterate over each space on the board to create the divs and assign the classes
+			i = 0,
+			// var space will hold the divs with the class space; var rows will be assigned the numbers of rows on the board; var columns will be assigned the number of columns on the board;
+			space, rows, columns;
 
 	// clears the board div before the spaces are built to prevent any issues when the spaces are assigned during a board rebuild
 	board.empty();
